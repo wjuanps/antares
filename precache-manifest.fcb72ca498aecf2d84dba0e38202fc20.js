2d01b18bf7b372c896e8fd7311d7198dddc1e954
Juan Soares <wjuan.ps@gmail.com>
2020-04-17 23:05:05 -0300

    Updates

---
+++ precache-manifest.fcb72ca498aecf2d84dba0e38202fc20.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829ee3a23630113f010cad96f99ef772",
+    "url": "/antares/index.html"
+  },
+  {
+    "revision": "3fbadb537ba1f51f7661",
+    "url": "/antares/static/css/2.a8683939.chunk.css"
+  },
+  {
+    "revision": "4d97db5d72a5bf9ccda8",
+    "url": "/antares/static/css/main.9ee2d0a3.chunk.css"
+  },
+  {
+    "revision": "3fbadb537ba1f51f7661",
+    "url": "/antares/static/js/2.46652ec2.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/antares/static/js/2.46652ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d97db5d72a5bf9ccda8",
+    "url": "/antares/static/js/main.2d45212d.chunk.js"
+  },
+  {
+    "revision": "f047494f61677845a36b",
+    "url": "/antares/static/js/runtime-main.5de750d2.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/antares/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/antares/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/antares/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/antares/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/antares/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "7e891757b6017b802bb0b757406da05c",
+    "url": "/antares/static/media/bg-girl-with-laptop.7e891757.png"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/antares/static/media/bg-masthead.20cbd480.jpg"
+  },
+  {
+    "revision": "3032d9c1447e89df3cefc95ed7770069",
+    "url": "/antares/static/media/bg-success.3032d9c1.png"
+  },
+  {
+    "revision": "e1a8b9bc6c336df4eea21146c907ea86",
+    "url": "/antares/static/media/bg-technology.e1a8b9bc.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/antares/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/antares/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/antares/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/antares/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/antares/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
